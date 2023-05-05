d14eaf2610e27c4cada5e6bbe23c84be274db0cd
Exorcist V <dungnguyenx2077@gmail.com>
2023-05-05 21:55:24 +0700

    abc

--- src/components/DefaultLayout/Contact/index.js
+++ src/components/DefaultLayout/Contact/index.js
@@ -1,46 +1,23 @@
-import classNames from 'classnames';
-import styles from './Contact.modules.scss';
-
+import classNames from 'classnames/bind';
+import styles from './Contact.module.scss';
 const cx = classNames.bind(styles);
 function Contact() {
-    let [data, setData] = useState({
-        email: '',
-    });
-    let { email } = data;
-    let [check, setCheck] = useState('');
-
-    let handleChange = (e) => {
-        let mail = e.target.value;
-        if (!mail.startsWith(' ')) {
-            setData({ ...data, [e.target.name]: [e.target.value] });
-        }
-        console.log(mail);
-    };
-
-    let pushData = (e) => {
-        e.preventDefault();
-        if (!data.email) {
-            setCheck('Vui lòng nhập đủ thông tin trước khi gửi!');
-        } else {
-            alert(data.email);
-        }
-    };
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('content-left')}>
-                <h2>ĐĂNG KÝ NHẬN THÔNG TIN</h2>
-            </div>
-
-            <div className={cx('content-right')}>
-                <div className={cx('input-email')}>
-                    <input type="email" placeholder="Email..." className={cx('email')} />
+            <div className={cx('content')}>
+                <div>
+                    <h2>ĐĂNG KÝ NHẬN THÔNG TIN</h2>
                 </div>
-                <div className={cx('btn-sign-up')}>
-                    <input type="submit" value="Đăng ký" className={cx('sign-up')} />
+                <div className={cx('content-right')}>
+                    <div className={cx('input-email')}>
+                        <input type="email" placeholder="Email..." className={cx('email')} />
+                    </div>
+                    <div className={cx('btn-sign-up')}>
+                        <input type="button" value="Đăng ký" className={cx('sign-up')} />
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-
 export default Contact;
